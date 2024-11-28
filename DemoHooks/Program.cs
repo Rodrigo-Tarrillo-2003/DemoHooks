cfcc2aa7ad93b3439961473a061e6e25bb9e12d2
ITLER TARRILLO <rodrigoiy52@gmail.com>
2024-11-28 10:47:09 -0500

    Demostracion de hooks formato de codigo

--- DemoHooks/Program.cs
+++ DemoHooks/Program.cs
@@ -1,1 +1,11 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Hola, Mundo!");
+Console.WriteLine("Prueba de formateo de codigo");
+int edad = 25;
+if (edad >= 18)
+{
+    Console.WriteLine("Es mayor de edad ");
+                            }
+                                    else
+{
+    Console.WriteLine("Es menor de edad");
+            }
